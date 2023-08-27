385fae0ad3d9f4f7c10bb8e384c4f07bf507c06c
Kelvin Souza <kelvinsouza2017@hotmail.com>
2023-08-27 19:17:37 -0300

    Desenvolvimento de método: 'Solicitar emprestimo consignado'

--- src/java/packages/ClientLP.java
+++ src/java/packages/ClientLP.java
@@ -2,5 +2,12 @@ package java.packages;
 
 public class ClientLP extends Client {
 	
+	public String cpf;
 	
+	public void applyForPayrollLoan(double value) {
+		double valueLimit = super.getLimit();
+		if(valueLimit < value) {
+			super.currentAccountBalance += value;
+		}
+	}
 }
