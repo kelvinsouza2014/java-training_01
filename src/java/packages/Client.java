3af8c94e4f111dde2da4807587ebcb91715ebcdb
Kelvin Souza <kelvinsouza2017@hotmail.com>
2023-08-27 19:16:20 -0300

    Adição de comentários

--- src/java/packages/Client.java
+++ src/java/packages/Client.java
@@ -1,11 +1,11 @@
 package java.packages;
 
 public class Client {
-
+	// ATTRIBUTES
 	public int agence;
 	public String checkingAccount;
 	public double currentAccountBalance;
-
+	// METHODS
 	public void withdrawCheckingAccount(double value) {
 		if(this.currentAccountBalance >= value) {
 			this.currentAccountBalance -= value;
