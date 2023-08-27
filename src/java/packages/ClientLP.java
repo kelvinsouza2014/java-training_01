a0a5984a6103252360b3a404363f6180a2537768
Kelvin Souza <kelvinsouza2017@hotmail.com>
2023-08-27 19:23:18 -0300

    Correção de conteúdo das classes

--- src/java/packages/ClientLP.java
+++ src/java/packages/ClientLP.java
@@ -2,12 +2,4 @@ package java.packages;
 
 public class ClientLP extends Client {
 	
-	public String cpf;
-	
-	public void applyForPayrollLoan(double value) {
-		double valueLimit = super.getLimit();
-		if(valueLimit < value) {
-			super.currentAccountBalance += value;
-		}
-	}
 }
--- src/java/packages/ClientPP.java
+++ src/java/packages/ClientPP.java
@@ -1,6 +1,13 @@
 package java.packages;
 
 public class ClientPP extends Client {
-
 	
+	public String cpf;
+	
+	public void applyForPayrollLoan(double value) {
+		double valueLimit = super.getLimit();
+		if(valueLimit < value) {
+			super.currentAccountBalance += value;
+		}
+	}
 }
